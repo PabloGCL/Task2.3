9747358cee6815bfce5c61bdd9f4983389396d10
PabloGCL <the_gilca@hotmail.com>
2018-11-20 10:19:01 +0100

    Task2.3_20Nov

--- Task2_3.R
+++ Task2_3.R
@@ -10,32 +10,32 @@ pacman::p_load(caret, lattice, ggplot2, ModelMetrics, mlbench, RcppRoll, bindrcp
 
 setwd("C:/Users/Usuario/Desktop/Big Data/2.3") 
 
-OGexisting <- read_delim("existingproductattributes2017.2.csv",",", escape_double = FALSE, trim_ws = TRUE)
+existprod <- read_delim("existingproductattributes2017.2.csv",",", escape_double = FALSE, trim_ws = TRUE)
 
-OGnew <- read_delim("newproductattributes2017.2.csv",",", escape_double = FALSE, trim_ws = TRUE)
+newprod <- read_delim("newproductattributes2017.2.csv",",", escape_double = FALSE, trim_ws = TRUE)
 
 
 ################ Product Size #####################
 
-OGexisting$ProductSize <- OGexisting$ProductHeight*OGexisting$ProductWidth*OGexisting$ProductDepth
-OGnew$ProductSize <- OGnew$ProductHeight*OGnew$ProductWidth*OGnew$ProductDepth
+existprod$ProductSize <- existprod$ProductHeight*existprod$ProductWidth*existprod$ProductDepth
+newprod$ProductSize <- newprod$ProductHeight*newprod$ProductWidth*newprod$ProductDepth
 
-OGexisting$ProductHeight <- NULL
-OGexisting$ProductWidth <- NULL
-OGexisting$ProductDepth <- NULL
+existprod$ProductHeight <- NULL
+existprod$ProductWidth <- NULL
+existprod$ProductDepth <- NULL
 
-OGnew$ProductHeight <- NULL
-OGnew$ProductWidth <- NULL
-OGnew$ProductDepth <- NULL
+newprod$ProductHeight <- NULL
+newprod$ProductWidth <- NULL
+newprod$ProductDepth <- NULL
 
 
 ################ Warranty & Rest ####################
 
-Warr <- filter(OGexisting, ProductType=="ExtendedWarranty")
-NoWarr <- filter(OGexisting, ProductType!="ExtendedWarranty")
+Warr <- filter(existprod, ProductType=="ExtendedWarranty")
+NoWarr <- filter(existprod, ProductType!="ExtendedWarranty")
 
-NewWarr <- filter(OGnew, ProductType=="ExtendedWarranty")
-NewNoWarr <- filter(OGnew, ProductType!="ExtendedWarranty")
+NewWarr <- filter(newprod, ProductType=="ExtendedWarranty")
+NewNoWarr <- filter(newprod, ProductType!="ExtendedWarranty")
 
 
 ################## Dummify the data ######################
@@ -67,10 +67,10 @@ corrNewDummy <- cor(newDummy)
 corrplot(corrNewDummy)
 
   ## Before Dummy 
-OGexisting$BestSellersRank <- NULL
-OGexisting$x5StarReviews <- NULL
-OGexisting$ProductType <- NULL
-OGexisting$ProductNum <- NULL
+existprod$BestSellersRank <- NULL
+existprod$x5StarReviews <- NULL
+existprod$ProductType <- NULL
+existprod$ProductNum <- NULL
 
 NewNoWarr$BestSellersRank <- NULL
 NewNoWarr$x5StarReviews <- NULL
@@ -82,7 +82,7 @@ NoWarr$x5StarReviews <- NULL
 NoWarr$ProductType <- NULL
 NoWarr$ProductNum <- NULL
 
-corrOGExist <- cor(OGexisting) 
+corrOGExist <- cor(existprod) 
 corrplot(corrOGExist)
 
 corrNew <- cor(NewNoWarr) 
@@ -142,38 +142,6 @@ control <- trainControl(method = "repeatedcv",
                         number = 10, 
                         repeats = 3,
                         classProbs=TRUE)
-##----------------------
-LModel <- list()
-for(i in 12:ncol(training)){
-  if(is.numeric(training[,i])){
-    LinearModel1 <- train(Volume ~ 0+training[,i], 
-                          data=training,
-                          method="lm",
-                          tuneLength=10
-                          #trControl=control
-                          )
-    PredLM1 <- predict(LinearModel1, testing)
-    metrics <- postResample(PredLM1, testing)
-    #metrics.acum <- cbind(metrics.acum, metrics)
-}
-}
-
-#RFmodel <- list()
-#for(i in 12:ncol(training)){
- # if(is.numeric(training[,i])){
-    RFmodel <- train(Volume ~ x4StarReviews+PositiveServiceReview, 
-                          data=training,
-                          method="rf",
-                          tuneLength=10,
-                          trControl=control
-    )
-    PredRF <- predict(RFmodel, testing)
-    RFmetrics <- postResample(PredRF, testing)
-    RFmetrics.acum <- cbind(metrics.acum, metrics)
-  #}
-#}
-
-##------------
 
     
   # One Variable #  
@@ -189,30 +157,12 @@ for(i in 12:(ncol(training)-1)){
   }
 }
 
+metrics.acumLM1
 #names(metrics.acumLM1)[,1:ncol(metrics.acumLM1)] <- colnames(training[1:(ncol(training)-1)])
 
 
-PredictionModels <- function(VBLEpredicted, VBLEnvariables){
-  
-  LModel <- train(as.formula(paste(c(VBLEpredicted, "~ 0+", temp[i]))), training, method=VBLEmethod)
-}
-
-
-metrics.acumRF <-  c()
-#for(i in 12:(ncol(training)-1)){
-  #if(is.numeric(training[,i])){
-    RFModel <- train(Volume ~., 
-                     data=training, method="rf", 
-                     ntrees=100, tuneLength=10, trControl=control, importance= TRUE)
-    PredRF <- predict(RFModel, testing)
-    metricsRF<-postResample(PredRF, testing$Volume)
-    metrics.acumRF<-cbind(metrics.acumRF, metricsRF)
-    importance(PredRF)
-#  }
-#}
-
-
 # Two Variables #
+
 temp <- names(training[,1:(ncol(training)-1)]) 
 metrics.acumLM2 <-  c()   
 set.seed(123)
@@ -228,12 +178,41 @@ for(i in 12:ncol(training)){
  }
 }
 
-predict(LinearModel2, testing)
-<-postResample()
-<-cbind()
 
-LMmodel1
-LinearModel1 <- lm(Volume ~ ., existing)
+
+metrics.acumRF <-  c()
+#for(i in 12:(ncol(training)-1)){
+#if(is.numeric(training[,i])){
+RFModel <- train(Volume ~., 
+                 data=training, method="rf", 
+                 ntrees=100, tuneLength=10, trControl=control, importance= TRUE)
+PredRF <- predict(RFModel, testing)
+metricsRF<-postResample(PredRF, testing$Volume)
+metrics.acumRF<-cbind(metrics.acumRF, metricsRF)
+importance(PredRF)
+#  }
+#}
+
+
+
+#RFmodel <- list()
+#for(i in 12:ncol(training)){
+# if(is.numeric(training[,i])){
+RFmodel <- train(Volume ~ x4StarReviews+PositiveServiceReview, 
+                 data=training,
+                 method="rf",
+                 tuneLength=10,
+                 trControl=control
+)
+PredRF <- predict(RFmodel, testing)
+RFmetrics <- postResample(PredRF, testing)
+RFmetrics.acum <- cbind(metrics.acum, metrics)
+#}
+#}
+
+
+
+
 
 summary(LinearModel)
 coefficients(LinearModel) # model coefficients
@@ -247,50 +226,3 @@ influence(LinearModel) # regression diagnostics
 ggplot(existing, aes(x=x4StarReviews, y=Volume)) +geom_point() + geom_smooth(method='lm', se=FALSE)
 
 
-################## Plots #######################
-
-for(i in 1:ncol(existing)){
-  if(is.numeric(existing[,i])){
-    qqnorm(existing[,i],main=paste("Normal Q-Qplot of", colnames(existing)[i])) #survey[,i] to retrieve column i 
-    qqline(existing[,i], col="red") #add qqnormal line
-    hist(existing[,i], main=paste("Histogram of", colnames(existing)[i]),xlab=colnames(existing)[i])
-  }
-}
-
-scatter.smooth(x=cars$speed, y=cars$dist, main="Dist ~ Speed")  # scatterplot
-
-par(mfrow=c(1, 2))  # divide graph area in 2 columns
-boxplot(cars$speed, main="Speed", sub=paste("Outlier rows: ", boxplot.stats(cars$speed)$out))  # box plot for 'speed'
-boxplot(cars$dist, main="Distance", sub=paste("Outlier rows: ", boxplot.stats(cars$dist)$out))  # box plot for 'distance'
-
-
-
-
-for (i in 13:ncol(existing)){
-  plot_ly(existing[i], main=paste('Boxplot of', colnames(existing[i]),'in regards to Volume'), type="box")
-  }
-
-plot_ly(y=NoWarr$Price, type="box")
-
-plot_ly(type="box") %>%
-add_boxplot(y = existing[27], jitter = 0.3, pointpos = -1.8, boxpoints = 'all',
-            marker = list(color = 'rgb(7,40,89)'),
-            line = list(color = 'rgb(7,40,89)'),
-            name = "Boxplot of in regards to Volume") %>%
-  layout(title = "Box Plot Styling Outliers")
-
-
-################# Models ##################
-
-inTrain <- createDataPartition(
-  y = NoWarr$Volume,
-  p = .75,
-  list = FALSE)
-
-str(inTrain)
-
-training <- NoWarr[ inTrain,]
-testing  <- NoWarr[-inTrain,]
-
-nrow(training)
-nrow(testing)
